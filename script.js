1047cb70020b84d9505102652e05801a06758804
maryam <marychch19@gmail.com>
2024-09-24 22:23:20 +0330

    exercise

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+//js exercise
+// const sentence = "the quick brown fox jumps over the lazy dog";
+// const reversSentence = sentence.split("").reverse().join("");
+// console.log(reversSentence);
+
+/**
+ *
+ *
+ *
+ *
+ */
